b05be7af93fd561eed043a45e0e915ff3aa2a48f
Hunter S <67876717+hunterschep@users.noreply.github.com>
2024-11-07 14:42:36 -0500

    update api.js

--- eagle-task/src/api/api.js
+++ eagle-task/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // Base URL for FastAPI backend
-const BASE_URL = 'http://localhost:8000'; // Update this URL if needed
+const BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://eagletask.onrender.com/' 
+  : 'http://127.0.0.1:8000/';
 
 /**
  * Sends a POST request to the FastAPI backend to retrieve courses and their grades.
