b284f29891b228e64a2aa38f7383d307fb7f4418
Sahil Saoji <68128782+sahilsaoji@users.noreply.github.com>
2024-11-07 12:46:20 -0500

    should deploy

--- eagle-task/src/components/Navbar.jsx
+++ eagle-task/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-//import { Link, useNavigate } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 
 function Navbar({ isLoggedIn, onSignOut }) {
