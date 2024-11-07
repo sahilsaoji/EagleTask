0a04cf0e31c4bf9af54e6c028c802f0ffa500bb4
Sahil Saoji <68128782+sahilsaoji@users.noreply.github.com>
2024-11-07 12:44:04 -0500

    warnings are not errors

--- eagle-task/src/components/Navbar.jsx
+++ eagle-task/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+//import { Link, useNavigate } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 
 function Navbar({ isLoggedIn, onSignOut }) {
